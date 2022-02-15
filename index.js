3764c6fb552112d8faf9d99f1192e6292cd37f5f
Gukkey <poetguhi@gmail.com>
2022-02-15 21:52:21 +0530

    test

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-const dotenv = require("dotenv");
-const { Client, Intents } = require("discord.js");
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
-const { commands } = require("./commands.js");
-dotenv.config();
-const token = process.env.TOKEN;
-const clientId = process.env.CLIENT_ID;
-const guildId = process.env.GUILD_ID;
-//test
-const rest = new REST({ version: "9" }).setToken(token);
+const dotenv = require('dotenv')
+const { Client, Intents } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { commands } = require('./commands.js')
+dotenv.config()
+const token = process.env.TOKEN
+const clientId = process.env.CLIENT_ID
+const guildId = process.env.GUILD_ID
+//main branch
+const rest = new REST({ version: '9' }).setToken(token);
 
-rest
-  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-  .then(() => console.log("Successfully registered application commands."))
-  .catch(console.error);
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
-client.once("ready", () => {
-  console.log("Welcome Gukkey and Shiyaam 👋");
+client.once('ready', () => {
+	console.log('Welcome Gukkey and Shiyaam 👋'); 
 });
 
-client.on("interactionCreate', async interaction => {
+client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
 	const { commandName } = interaction;
