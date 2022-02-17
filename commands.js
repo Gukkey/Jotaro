49c06c5cf2c4a47c551093d3f265ec8c190dd2dc
shiyaam <shiyaamsunder@users.noreply.github.com>
2022-02-17 13:15:54 +0530

    added warn command

--- commands.js
+++ commands.js
@@ -2,16 +2,15 @@ module.exports = {
   commands: [
     {
       name: "ping",
-      description: "Replies with Pong!"
+      description: "Replies with Pong!",
     },
     {
       name: "donate",
-      description:
-        'Gives a "Buy me a coffee link" to donate to devs for this project' //some commands like this one and the commands like open source are not neccessary for moderation or multi utility but for our own learning process and donation jar.
+      description: 'Gives a "Buy me a coffee link" to donate to devs for this project', // some commands like this one and the commands like open source are not neccessary for moderation or multi utility but for our own learning process and donation jar.
     },
     {
       name: "source",
-      description: "Provides the source code for this bot"
+      description: "Provides the source code for this bot",
     },
     {
       name: "ban",
@@ -21,9 +20,15 @@ module.exports = {
           name: "user",
           description: "Enter User name",
           required: true,
-          type: 6
-        }
-      ]
+          type: 6,
+        },
+        {
+          name: "reason",
+          description: "Reason for kick",
+          required: true,
+          type: 3,
+        },
+      ],
     },
     {
       name: "kick",
@@ -33,9 +38,33 @@ module.exports = {
           name: "user",
           description: "Enter User name",
           required: true,
-          type: 6
-        }
-      ]
-    }
-  ]
+          type: 6,
+        },
+        {
+          name: "reason",
+          description: "Reason for kick",
+          required: false,
+          type: 3, // type 3 ordinary string
+        },
+      ],
+    },
+    {
+      name: "warn",
+      description: "Warn an user",
+      options: [
+        {
+          name: "user",
+          description: "Enter User name",
+          required: true,
+          type: 9, // type 6 member type
+        },
+        {
+          name: "reason",
+          description: "Enter a reason for the warning",
+          required: false,
+          type: 3,
+        },
+      ],
+    },
+  ],
 };
